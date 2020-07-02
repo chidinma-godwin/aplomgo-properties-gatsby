b8ddad80ee49bc4a221400bf091ddb0656dd4aa0
Chidinma Godwin <gchidex@yahoo.com>
2020-07-02 06:24:09 +0100

    Import font awesome icons that will be used

---
+++ src/fontawesome.js
@@ -0,0 +1,30 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faMapMarkerAlt,
+  faPhoneAlt,
+  faEnvelope,
+  faBars,
+  faComment,
+  faCaretDown,
+  faDownload,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookSquare,
+  faTwitterSquare,
+  faInstagramSquare,
+  faWhatsappSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faMapMarkerAlt,
+  faPhoneAlt,
+  faFacebookSquare,
+  faEnvelope,
+  faTwitterSquare,
+  faInstagramSquare,
+  faWhatsappSquare,
+  faBars,
+  faComment,
+  faCaretDown,
+  faDownload
+);
