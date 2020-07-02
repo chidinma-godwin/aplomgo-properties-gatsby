9f7c3dcf0d839d1aa10a171e85b5682ebd1b6941
Chidinma Godwin <gchidex@yahoo.com>
2020-07-02 06:21:38 +0100

    Add the properties data

---
+++ src/data/questions.js
@@ -0,0 +1,31 @@
+const getQuestions = name => [
+  { id: 1, question: `Where Is ${name}?` },
+  { id: 2, question: `Who Is the Developer of ${name}?` },
+  { id: 3, question: `What Type of Title Does ${name} Have on The Land?` },
+  { id: 4, question: 'Are They Any Encumbrances on The Land?' },
+  { id: 5, question: 'What Is the Payment Structure?' },
+  { id: 6, question: 'What Is the Size of The Land?' },
+  { id: 7, question: 'Is the Road to The Estate Motorable?' },
+  { id: 8, question: 'What do I Get after the PAYMENT?' },
+  { id: 9, question: 'What Do I Get After Completing Payment of The Land?' },
+  { id: 10, question: 'Can I start Construction or Building on The Land Now?' },
+  { id: 11, question: 'Is There Any Time Limit to Commence on My Land After Allocation?' },
+  {
+    id: 12,
+    question:
+      'Is There Any Restriction Regarding the Type of Building I Can Construct in The Estate?',
+  },
+  { id: 13, question: 'Can I Re-Sell My Plot/Property?' },
+  { id: 14, question: 'Can I Pay Cash to Your Agent?' },
+  {
+    id: 15,
+    question: 'If I Paid Outright for My Plot(S) And I`M No Longer Interested Can I Get A Refund?',
+  },
+  {
+    id: 16,
+    question:
+      'If I Was on Installment Payment Plan and Cannot Continue with The Other Payment, Can I Get A Re-Fund?',
+  },
+];
+
+export default getQuestions;
