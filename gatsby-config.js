33c29db809d01fa900b647ea510d231a93849386
Chidinma Godwin <gchidex@yahoo.com>
2020-07-02 06:25:33 +0100

    Customise configs that came with the default starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,21 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aplomgo Real Estate`,
+    description: `Aplom Go is one of Nigeria’s indigenous Real Estate
+                  development Company with a vision to carry out real estate professional services and to
+                  deliver competitive real estate solutions to our highly esteemed
+                  clients.`,
+    siteUrl: "http://aplomgo.com",
+    image: "/images/blackLogo.jpg",
+    author: `Aplom Go. Limited`,
+    email: "info@aplomgo.com",
+    social: {
+      facebook: "https://www.facebook.com/Aplomhomes",
+      instagram: "https://instagram.com/aplomgo_aplomgo",
+      twitter: "https://twitter.com/aplomgo",
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,23 +23,32 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Aplomgo Real Estate and Development`,
+        short_name: `Aplomgo Properties`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.jpg`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-layout`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,69 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const properties = require("./src/data/properties.json")
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "PropertiesJson") {
+    createNodeField({
+      node,
+      name: `propertyImage`,
+      value: `../images/${node.image}`,
+    })
+  }
+}
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    {
+      allPropertiesJson {
+        nodes {
+          answers {
+            ans
+            id
+          }
+          id
+          infras
+          description
+          spots
+          legals
+          location
+          name
+          shortName
+          price
+          fields {
+            propertyImage {
+              childImageSharp {
+                fluid(maxWidth: 1000) {
+                  base64
+                  aspectRatio
+                  src
+                  srcSet
+                  sizes
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const allProperties = result.data.allPropertiesJson.nodes
+
+  allProperties.forEach(property => {
+    createPage({
+      path: `/properties/${property.shortName}`,
+      component: require.resolve(`./src/templates/properties.js`),
+      context: { property },
+    })
+  })
+
+  allProperties.forEach(subscriptionProperty => {
+    createPage({
+      path: `/subscription/${subscriptionProperty.shortName}`,
+      component: require.resolve(`./src/templates/subscription.js`),
+      context: { subscriptionProperty },
+    })
+  })
+}
